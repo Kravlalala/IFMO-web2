9cb5ceabd6efd8c54836341ac94268504754614a
Ilya <kravlala.1@gmail.com>
2016-11-28 00:56:35 +0800

    Registration with redirect

---
+++ src/Exceptions/IDException.java
@@ -0,0 +1,11 @@
+package Exceptions;
+
+/**
+ * Created by kravlala on 21.11.16.
+ */
+
+public class IDException extends Exception {
+    public IDException(String message){
+        super(message);
+    }
+}
---
+++ src/Servletes/SignInServlet.java
@@ -0,0 +1,18 @@
+package Servletes;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by kravlala on 20.11.16.
+ */
+public class SignInServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
